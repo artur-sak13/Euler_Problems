185f37d975157221185eeec83e3a4ddc30c3aeb4
artur-sak13 <artursak1981@gmail.com>
2017-12-01 03:42:58 -0600

    Organizing and problem 11

--- Euler1.py
+++
@@ -1,1 +0,0 @@
-sum=0for n in range(1,1000):    if n%3==0:        sum=sum+n    if n%5==0:        sum=sum+n    if n%15==0:        sum=sum-nprint(sum)
--- Euler16.rb
+++
@@ -1,7 +0,0 @@
-sum = 0
-number = (2**1000)
-
-number = number.to_s.split('')
-(number).each{|x| sum += x.to_i}
-
-puts sum
